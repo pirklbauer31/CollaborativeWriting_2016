85e7fa9d010bea90e5f205c5abacf892c6c3d0e2
pirklbauer31 <Kevin1995>
2016-06-18 13:27:27 +0200

    Some minor changes

---
+++ app/src/main/java/fh/mc/collaborativewriting/FriendlistActivity.java
@@ -0,0 +1,14 @@
+package fh.mc.collaborativewriting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FriendlistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friendlist);
+
+    }
+}
