6c97036870f168cc99569837f4a06a6ff66cbee7
moly94 <felix.mauler@gmail.com>
2016-07-02 18:10:13 +0200

    Fixed Profile Pic saving back to original Version with no compression

--- app/src/main/java/fh/mc/collaborativewriting/MainActivity.java
+++ app/src/main/java/fh/mc/collaborativewriting/MainActivity.java
@@ -459,8 +459,10 @@ public class MainActivity extends BaseActivity
                         yourSelectedImage = decodeUri(selectedImage);
                         saveImageLocal(yourSelectedImage, selectedImage);
                         Log.d(TAG,Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getAbsolutePath().toString() + "/Project_CoW/"+selectedImage.getLastPathSegment());
-                        uploadFromUri(Uri.parse(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getAbsolutePath().toString() + "/Project_CoW/"+selectedImage.getLastPathSegment()+".PNG"));
-                        updateProfilePic(Uri.parse(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getAbsolutePath().toString() + "/Project_CoW/"+selectedImage.getLastPathSegment()+".PNG"));
+                        //uploadFromUri(Uri.parse(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getAbsolutePath().toString() + "/Project_CoW/"+selectedImage.getLastPathSegment()+".PNG"));
+                        //updateProfilePic(Uri.parse(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getAbsolutePath().toString() + "/Project_CoW/"+selectedImage.getLastPathSegment()+".PNG"));
+                        uploadFromUri(selectedImage);
+                        updateProfilePic(selectedImage);
                         img.setImageBitmap(yourSelectedImage);
 
 
